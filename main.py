d6fcef0637a2ef9d38e404d58565c2328122945f
elkhayyat <ahmed.elkhayyat@onlink4it.com>
2023-09-25 02:59:42 +0300

    refinement and adding dialog

--- main.py
+++ main.py
@@ -3,20 +3,44 @@ import os
 from dotenv import load_dotenv
 
 from postgres import PostgresConnection
-from remote import SSHConnection
 from zabbix_setter import ZabbixHostNameSetter
 
 load_dotenv()
 
 
-def main():
-    db_host = os.getenv('DB_HOST')
-    db_name = os.getenv('DB_NAME')
-    db_user = os.getenv('DB_USER')
-    db_password = os.getenv('DB_PASSWORD')
-    db_port = os.getenv('DB_PORT')
-    postgres_connection = PostgresConnection(db_host=db_host, db_name=db_name, db_user=db_user,
-                                             db_password=db_password, db_port=db_port)
-    postgres_connection.connect()
-    setter = ZabbixHostNameSetter(connection=postgres_connection)
-    setter.run_for_certain_host_id(host_id=10582)
+class Main:
+    def __init__(self):
+        self.zabbix_host_name_setter = None
+
+    def get_postgres_connection(self):
+        db_host = os.getenv('DB_HOST')
+        db_name = os.getenv('DB_NAME')
+        db_user = os.getenv('DB_USER')
+        db_password = os.getenv('DB_PASSWORD')
+        db_port = os.getenv('DB_PORT')
+        postgres_connection = PostgresConnection(db_host=db_host, db_name=db_name, db_user=db_user,
+                                                 db_password=db_password, db_port=db_port)
+        return postgres_connection
+
+    def main(self):
+        connection = self.get_postgres_connection()
+        zabbix_host_name_setter = ZabbixHostNameSetter(connection=connection)
+        print("=" * 100)
+        print("Starting zabbix host name setter")
+        print("=" * 100)
+        print("1. Set host name to system name for all hosts")
+        print("2. Set host name to system name for certain host")
+        print("Choose action:")
+        action = input()
+        if action == "1":
+            zabbix_host_name_setter.run()
+        elif action == "2":
+            print("Enter host id:")
+            host_id = input()
+            zabbix_host_name_setter.run_for_certain_host_id(host_id)
+        else:
+            print("Wrong action")
+            exit(1)
+
+
+Main().main()
--- zabbix_setter.py
+++ zabbix_setter.py
@@ -32,22 +32,7 @@ class ZabbixHostNameSetter:
         hosts = self.get_hosts()
         for host in hosts:
             host_id = host[0]
-            host_name = host[1]
-            item_id = self.get_host_system_name_item_id(host_id)
-            if item_id:
-                item_id = item_id[0]
-                host_system_name = self.get_host_system_name(item_id)
-                if host_system_name:
-                    host_system_name = host_system_name[0]
-                    if host_system_name != host_name:
-                        self.set_host_name_to_host_system_name(host_id, host_system_name)
-                        print('Host %s name changed to %s' % (host_name, host_system_name))
-                    else:
-                        print('Host %s name is already %s' % (host_name, host_system_name))
-                else:
-                    print('Host %s has no system name' % host_name)
-            else:
-                print('Host %s has no system name item' % host_name)
+            self.run_for_certain_host_id(host_id)
 
     def run_for_certain_host_id(self, host_id):
         host = self.get_host_by_host_id(host_id)
