baf84fa23980da033b8dedbcb7b3f47a7670cae1
elkhayyat <ahmed.elkhayyat@onlink4it.com>
2023-09-25 04:12:28 +0300

    improve reporting

--- zabbix_setter.py
+++ zabbix_setter.py
@@ -2,6 +2,13 @@ class ZabbixHostNameSetter:
     def __init__(self, connection):
         self.connection = connection
         self.count = 0
+        self.summary_counter = {
+            "total": 0,
+            "changed": 0,
+            "already_has_system_name": 0,
+            "has_no_system_name": 0,
+            "has_no_system_name_item": 0
+        }
 
     def get_hosts(self):
         self.connection.connect()
@@ -35,9 +42,16 @@ class ZabbixHostNameSetter:
             self.count += 1
             host_id = host[0]
             self.run_for_certain_host_id(host_id)
+        self.summary_counter["total"] = self.count
         print("=" * 100)
-        print("Total hosts: %s" % self.count)
         print("Done")
+        print("=" * 100)
+        print(f"Total hosts: {self.summary_counter.get('total')}")
+        print(f"Changed hosts: {self.summary_counter.get('changed')}")
+        print(f"Already has system name hosts: {self.summary_counter.get('already_has_system_name')}")
+        print(f"Has no system name hosts: {self.summary_counter.get('has_no_system_name')}")
+        print(f"Has no system name item hosts: {self.summary_counter.get('has_no_system_name_item')}")
+        print("=" * 100)
 
     def run_for_certain_host_id(self, host_id):
         host = self.get_host_by_host_id(host_id)
@@ -50,10 +64,14 @@ class ZabbixHostNameSetter:
                 host_system_name = host_system_name[0]
                 if host_system_name != host_name:
                     self.set_host_name_to_host_system_name(host_id, host_system_name)
+                    self.progress_counter["changed"] += 1
                     print(f"{self.count}. Host {host_name} name changed to {host_system_name}")
                 else:
+                    self.progress_counter["already_has_system_name"] += 1
                     print(f"{self.count}. Host {host_name} name is already {host_system_name}")
             else:
+                self.progress_counter["has_no_system_name"] += 1
                 print(f"{self.count}. Host {host_name} has no system name")
         else:
+            self.progress_counter["has_no_system_name_item"] += 1
             print(f"{self.count}. Host {host_name} has no system name item")
